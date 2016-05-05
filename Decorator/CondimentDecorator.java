2d747c3f3ea72620ac20bac09e504fec3237cd06
syrilster <syrilster@gmail.com>
2016-05-05 17:44:59 +0530

    Create CondimentDecorator.java

---
+++ Decorator/CondimentDecorator.java
@@ -0,0 +1,8 @@
+package designPattern.DecoratorPattern;
+
+/**
+ * Created by syrils on 2/29/16.
+ */
+public abstract class CondimentDecorator extends Beverage{
+    public abstract String getDescription();
+}
