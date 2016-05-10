5df45d2db51b9f331277ef6ee62d4610dc4c7048
syrilster <syrilster@gmail.com>
2016-05-10 10:59:45 +0530

    Create PizzaStore.java

---
+++ Factory/PizzaStore.java
@@ -0,0 +1,21 @@
+package DesignPatterns.FactoryPattern;
+
+/**
+ * Created by Syril on 09-05-2016.
+ */
+public abstract class PizzaStore {
+
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+        pizza = createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.pack();
+
+        return pizza;
+    }
+
+    protected abstract Pizza createPizza(String type);
+}
