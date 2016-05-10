6092a9f6b78e3c56d632ce23bc32981e1201cffd
syrilster <syrilster@gmail.com>
2016-05-10 10:57:58 +0530

    Create ChicagoPizzaIngredientFactory.java

---
+++ Factory/ChicagoPizzaIngredientFactory.java
@@ -0,0 +1,16 @@
+package DesignPatterns.FactoryPattern;
+
+/**
+ * Created by Syril on 09-05-2016.
+ */
+public class ChicagoPizzaIngredientFactory implements PizzaIngredientFactory {
+    @Override
+    public Dough createDough() {
+        return new ThickCrustDough();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return new PlumTomatoSauce();
+    }
+}
