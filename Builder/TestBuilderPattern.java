c276bc89da63fb571a7d4d6e1222b30bc68afe94
syrilster <syrilster@gmail.com>
2016-05-10 17:54:22 +0530

    Create TestBuilderPattern.java

---
+++ Builder/TestBuilderPattern.java
@@ -0,0 +1,12 @@
+package designPattern.Builder;
+
+/**
+ * Created by syrils on 5/10/16.
+ */
+public class TestBuilderPattern {
+    public static void main(String[] args) {
+        Computer comp = new Computer.ComputerBuilder("500 GB", "2 GB").setBluetoothEnabled(true).build();
+
+        System.out.println(comp);
+    }
+}
