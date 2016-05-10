0814eda6601614bee6e4a3f06ece070f50f6da0f
syrilster <syrilster@gmail.com>
2016-05-10 17:13:44 +0530

    Create AbstractLogger.java

---
+++ ChainOfResponsibility/AbstractLogger.java
@@ -0,0 +1,32 @@
+package designPattern.ChainOfResponsibility;
+
+/**
+ * Created by syrils on 3/31/16.
+ */
+public abstract class AbstractLogger {
+
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
+    protected int level;
+    protected AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger logger) {
+        this.nextLogger = logger;
+    }
+
+    public void logMessage(int level, String message) {
+        if (this.level <= level) {
+            write(message);
+        }
+
+        if (nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+
+    }
+
+    protected abstract void write(String message);
+
+}
