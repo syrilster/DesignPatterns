d1f33411dad1777beeab3ddf408d5e42845d6c99
syrilster <syrilster@gmail.com>
2016-05-10 17:14:35 +0530

    Create ErrorLogger.java

---
+++ ChainOfResponsibility/ErrorLogger.java
@@ -0,0 +1,16 @@
+package designPattern.ChainOfResponsibility;
+
+/**
+ * Created by syrils on 4/1/16.
+ */
+public class ErrorLogger extends AbstractLogger {
+
+    ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error :: Logger "+ message);
+    }
+}
