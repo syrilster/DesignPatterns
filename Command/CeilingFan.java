73acc3c9faa040ba5907b33c44f759335779103e
syrilster <syrilster@gmail.com>
2016-05-11 19:04:26 +0530

    Update CeilingFan.java

--- Command/CeilingFan.java
+++ Command/CeilingFan.java
@@ -4,17 +4,39 @@ package designPattern.command;
  * Created by syrils on 5/11/16.
  */
 public class CeilingFan {
-    private String name;
+    public static final int HIGH = 3;
+    public static final int MEDIUM = 2;
+    public static final int LOW = 1;
+    public static final int OFF = 0;
+    private String location;
+    int speed;
 
     public CeilingFan(String name) {
-        this.name = name;
+        this.location = name;
+        speed = OFF;
     }
 
-    public void on() {
-        System.out.println(name + " Fan Switched on");
+    public void high() {
+        speed = HIGH;
+        System.out.println(location + " Fan is on HIGH");
+    }
+
+    public void low() {
+        speed = LOW;
+        System.out.println(location + " Fan is on LOW");
+    }
+
+    public void medium() {
+        speed = MEDIUM;
+        System.out.println(location + " Fan is on MEDIUM");
     }
 
     public void off() {
-        System.out.println(name + " Fan Switched off");
+        speed = OFF;
+        System.out.println(location + " Fan is OFF");
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 }
