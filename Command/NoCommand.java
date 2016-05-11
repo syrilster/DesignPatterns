93705beed06748b3023bd2ecc4b7238dc6c29539
syrilster <syrilster@gmail.com>
2016-05-11 18:02:05 +0530

    Create NoCommand.java

---
+++ Command/NoCommand.java
@@ -0,0 +1,11 @@
+package designPattern.command;
+
+/**
+ * Created by syrils on 5/11/16.
+ */
+public class NoCommand implements Command {
+    @Override
+    public void execute() {
+        
+    }
+}
