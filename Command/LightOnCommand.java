d7ce997babefdaa84c3f587814ead989716fe886
syrilster <syrilster@gmail.com>
2016-05-11 18:00:57 +0530

    Create LightOnCommand.java

---
+++ Command/LightOnCommand.java
@@ -0,0 +1,17 @@
+package designPattern.command;
+
+/**
+ * Created by syrils on 5/11/16.
+ */
+public class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.on();
+    }
+}
