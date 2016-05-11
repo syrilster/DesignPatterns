4e79ca20b19c90612dd85cc46cd9a860b8c63e79
syrilster <syrilster@gmail.com>
2016-05-11 19:05:10 +0530

    Update CeilingFanOffCommand.java

--- Command/CeilingFanOffCommand.java
+++ Command/CeilingFanOffCommand.java
@@ -3,15 +3,33 @@ package designPattern.command;
 /**
  * Created by syrils on 5/11/16.
  */
-public class CeilingFanOffCommand implements Command {
+public class CeilingFanOffCommand implements Command{
     CeilingFan ceilingFan;
+    int prevSpeed;
 
     public CeilingFanOffCommand(CeilingFan ceilingFan) {
         this.ceilingFan = ceilingFan;
     }
-
+    
+    
     @Override
     public void execute() {
-        ceilingFan.off();
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.off();    
+    }
+
+    @Override
+    public void undo() {
+        System.out.println("Undo previous command \n");
+        
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
     }
 }
