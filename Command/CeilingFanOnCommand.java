fd5269bcd9555f5dabe43a7accb7932a3895f91f
syrilster <syrilster@gmail.com>
2016-05-11 18:01:35 +0530

    Create CeilingFanOnCommand.java

---
+++ Command/CeilingFanOnCommand.java
@@ -0,0 +1,17 @@
+package designPattern.command;
+
+/**
+ * Created by syrils on 5/11/16.
+ */
+public class CeilingFanOnCommand implements Command {
+    CeilingFan ceilingFan;
+
+    public CeilingFanOnCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        ceilingFan.on();
+    }
+}
