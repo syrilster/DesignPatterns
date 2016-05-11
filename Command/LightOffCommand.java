c7c62a73ddac529631c8209dcc5446acdbd996f6
syrilster <syrilster@gmail.com>
2016-05-11 18:01:15 +0530

    Create LightOffCommand.java

---
+++ Command/LightOffCommand.java
@@ -0,0 +1,18 @@
+package designPattern.command;
+
+/**
+ * Created by syrils on 5/11/16.
+ */
+public class LightOffCommand implements Command {
+
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+}
