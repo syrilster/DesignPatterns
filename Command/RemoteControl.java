7a0e24a600e38cd25a43fad550dfc991898681e2
syrilster <syrilster@gmail.com>
2016-05-11 18:02:24 +0530

    Create RemoteControl.java

---
+++ Command/RemoteControl.java
@@ -0,0 +1,57 @@
+package designPattern.command;
+
+/**
+ * Created by syrils on 5/11/16.
+ */
+public class RemoteControl {
+    // on and off commands held in an array of Commands
+    Command[] onCommands;
+    Command[] offCommands;
+
+    public RemoteControl() {
+        onCommands = new Command[7];
+        offCommands = new Command[7];
+
+        // Null Object Pattern used here : NoCommand is and example of null object - when you dont have a meaningful 
+        // object to return, and yet you want to remove the responsibility for handling null from the client. 
+        Command noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
+    }
+
+    /**
+     * Takes the slot position and an on and off command to be stored in that slot. It puts these commands in the on
+     * and off arrays for later use.
+     *
+     * @param slot
+     * @param onCommand
+     * @param offCommand
+     */
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    //When on and off button is pressed, this method takes care of calling the corresponding execute methods.
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPushed(int slot) {
+        offCommands[slot].execute();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n ----- Remote Control ---- \n");
+        for (int i = 0; i < onCommands.length; i++) {
+            stringBuilder.append("[Slot " + i + "] " + onCommands[i].getClass().getName() + " " + offCommands[i]
+                    .getClass().getName() + "\n");
+        }
+
+        return stringBuilder.toString();
+    }
+}
