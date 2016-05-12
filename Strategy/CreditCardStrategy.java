7b7bbe3375c999b00f5a5b0d6dc727ec9c682462
syrilster <syrilster@gmail.com>
2016-05-12 15:11:21 +0530

    Create CreditCardStrategy.java

---
+++ Strategy/CreditCardStrategy.java
@@ -0,0 +1,24 @@
+package designPattern.StratergyPattern;
+
+/**
+ * Created by syrils on 3/28/16.
+ */
+public class CreditCardStrategy implements PaymentStrategy {
+
+    private int cardNumber;
+    private String name;
+    private int cvv;
+    private int pin;
+
+    CreditCardStrategy(int cardNumber, String name, int cvv, int pin) {
+        this.cardNumber = cardNumber;
+        this.name = name;
+        this.cvv = cvv;
+        this.pin = pin;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println(amount + " Made payment by using Credit Card");
+    }
+}
