b1659f1976052816fba569e1297f0c00c4ed24f9
syrilster <syrilster@gmail.com>
2016-05-13 15:26:30 +0530

    Create ChatMediatorImpl.java

---
+++ Mediator/ChatMediatorImpl.java
@@ -0,0 +1,30 @@
+package designPattern.Mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by syrils on 5/13/16.
+ */
+public class ChatMediatorImpl implements ChatMediator {
+
+    List<User> users;
+
+    ChatMediatorImpl() {
+        this.users = new ArrayList<>();
+    }
+
+    @Override
+    public void sendMessage(String message, User user) {
+        for (User u : users) {
+            if (u != user) {
+                u.receive(message);
+            }
+        }
+    }
+
+    @Override
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+}
