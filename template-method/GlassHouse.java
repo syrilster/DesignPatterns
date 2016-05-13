f54a59cc97f8dd39d7d36c78d99bd75b07efc5a7
syrilster <syrilster@gmail.com>
2016-05-13 17:05:58 +0530

    Create GlassHouse.java

---
+++ template-method/GlassHouse.java
@@ -0,0 +1,16 @@
+package designPattern.TemplateMethodPattern;
+
+/**
+ * Created by syrils on 2/29/16.
+ */
+public class GlassHouse extends HouseTemplate {
+    @Override
+    public void buildPillars() {
+        System.out.println("\n Building pillars coated with Glass");
+    }
+
+    @Override
+    public void buildWalls() {
+        System.out.println("\n Building walls coated with Glass");
+    }
+}
