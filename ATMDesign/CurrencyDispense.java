16fda4e549983d699f9432dfea88640178b7b687
syrilster <syrilster@gmail.com>
2016-05-23 10:58:56 +0530

    Create CurrencyDispense.java

---
+++ ATMDesign/CurrencyDispense.java
@@ -0,0 +1,9 @@
+package DesignPatterns.ATMDesign;
+
+/**
+ * Created by Syril on 17-05-2016.
+ */
+public interface CurrencyDispense {
+    void setNextChain(CurrencyDispense dispense);
+    void dispense(Currency currency);
+}
