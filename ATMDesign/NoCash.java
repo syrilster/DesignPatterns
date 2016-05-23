b1ba1e6ad2aee3c113a8ec1b1e7edd7672a67f8d
syrilster <syrilster@gmail.com>
2016-05-23 10:57:23 +0530

    Create NoCash.java

---
+++ ATMDesign/NoCash.java
@@ -0,0 +1,33 @@
+package DesignPatterns.ATMDesign;
+
+/**
+ * Created by Syril on 17-05-2016.
+ */
+public class NoCash implements ATMState {
+
+    ATMMachine atmMachine;
+
+    public NoCash(ATMMachine atmMachine) {
+        this.atmMachine = atmMachine;
+    }
+
+    @Override
+    public void insertCard() {
+        System.out.println("ATM is out of money");
+    }
+
+    @Override
+    public void ejectCard() {
+        System.out.println("ATM is out of money");
+    }
+
+    @Override
+    public void insertPin(int pinEntered) {
+        System.out.println("ATM is out of money");
+    }
+
+    @Override
+    public void requestCash(int amount) {
+        System.out.println("ATM is out of money");
+    }
+}
