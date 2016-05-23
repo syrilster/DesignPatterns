2dd663f60aa22213fb64f2b78ec4ed211aa44a68
syrilster <syrilster@gmail.com>
2016-05-23 10:58:36 +0530

    Create Currency.java

---
+++ ATMDesign/Currency.java
@@ -0,0 +1,16 @@
+package DesignPatterns.ATMDesign;
+
+/**
+ * Created by Syril on 17-05-2016.
+ */
+public class Currency {
+    private int amount;
+
+    public Currency(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
