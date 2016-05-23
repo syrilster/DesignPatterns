75788dcb057631d855519e2c531d37025b11614a
syrilster <syrilster@gmail.com>
2016-05-23 10:58:11 +0530

    Create ATMProxy.java

---
+++ ATMDesign/ATMProxy.java
@@ -0,0 +1,18 @@
+package DesignPatterns.ATMDesign;
+
+/**
+ * Created by Syril on 18-05-2016.
+ */
+public class ATMProxy implements GetATMData {
+    @Override
+    public ATMState getATMState() {
+        ATMMachine atmMachine = new ATMMachine();
+        return atmMachine.getATMState();
+    }
+
+    @Override
+    public int getCashInATMMachine() {
+        ATMMachine atmMachine = new ATMMachine();
+        return atmMachine.getCashInATMMachine();
+    }
+}
