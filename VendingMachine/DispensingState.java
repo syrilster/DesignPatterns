5fe8575f01fec7d00b442a07a7af4fafb3771325
syrilster <syrilster@gmail.com>
2016-07-19 11:21:05 +0530

    Update DispensingState.java

--- VendingMachine/DispensingState.java
+++ VendingMachine/DispensingState.java
@@ -1,7 +1,7 @@
-package DesignPatterns.VendingMachine;
+package designPattern.VendingMachine;
 
 /**
- * Created by Syril on 18-07-2016.
+ * Created by syrils on 7/19/16.
  */
 public class DispensingState implements VendingState {
     VendingMachine vendingMachine;
@@ -12,7 +12,7 @@ public class DispensingState implements VendingState {
 
     @Override
     public void insertCoin(int amount) {
-        System.out.println("Please while.. Machine is dispensing !!");
+        System.out.println("Please wait.. Machine is dispensing !!");
     }
 
     @Override
