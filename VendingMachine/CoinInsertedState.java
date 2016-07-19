78cab412ca36cffce1110b01ee88fa79fbf14c83
syrilster <syrilster@gmail.com>
2016-07-19 11:20:23 +0530

    Update CoinInsertedState.java

--- VendingMachine/CoinInsertedState.java
+++ VendingMachine/CoinInsertedState.java
@@ -1,7 +1,7 @@
-package DesignPatterns.VendingMachine;
+package designPattern.VendingMachine;
 
 /**
- * Created by Syril on 18-07-2016.
+ * Created by syrils on 7/19/16.
  */
 public class CoinInsertedState implements VendingState {
 
@@ -22,10 +22,13 @@ public class CoinInsertedState implements VendingState {
         int itemPrice = vendingMachine.getItemCost(slot);
         if (itemCapacity == 0) {
             System.out.println("Item Out of Stock");
+        } else if (vendingMachine.getDispenseCommand()[slot] instanceof NoCommand) {
+            System.out.println("Item Does not exist.. Please Try Again !!");
         } else if (vendingMachine.getAmountEntered() >= itemPrice) {
             vendingMachine.getDispenseCommand()[slot].execute();
             vendingMachine.setVendingState(vendingMachine.getDispensingState());
-            System.out.println("Tendering Change : " + (itemPrice - vendingMachine.getAmountEntered()));
+            if ((itemPrice - vendingMachine.getAmountEntered()) > 0)
+                System.out.println("Tendering Change : " + (itemPrice - vendingMachine.getAmountEntered()));
             vendingMachine.setVendingState(vendingMachine.getNoCoinInsertedState());
         } else {
             System.out.println("More Credits Required. Please insert currency value " + (itemPrice - vendingMachine.getAmountEntered()));
