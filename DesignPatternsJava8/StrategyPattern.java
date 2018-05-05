cf37e85e5ebcd04adb8429dd5aa13752f2faf923
Syril <syrilster@gmail.com>
2018-05-05 12:52:46 +0530

    Updates using functional programming.

--- DesignPatternsJava8/StrategyPattern.java
+++ DesignPatternsJava8/StrategyPattern.java
@@ -9,22 +9,28 @@ import static java.util.Arrays.asList;
  * Created by Syril on 05-05-2018.
  */
 public class StrategyPattern {
+    // Java 8 has lightweight strategy pattern unlike the heavy weight Java 7 way as there is a interface hierarchy.
+    // i.e. Use of library of classes over a set of function usage like the Util.isEven.
     public static void main(String[] args) {
         List<Integer> numbers = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(totalValues(numbers, e -> true));
         //System.out.println(totalEvenValues(numbers));
         //System.out.println(totalOddValues(numbers));
-        System.out.println(totalValues(numbers, e -> e % 2 == 0));
-        System.out.println(totalValues(numbers, e -> e % 2 != 0));
+        System.out.println(totalValues(numbers, Util::isEven));
+        System.out.println(totalValues(numbers, Util::isOdd));
     }
 
-    private static int totalValues(List<Integer> numbers, Predicate<Integer> condition) {
-        int result = 0;
+    private static int totalValues(List<Integer> numbers, Predicate<Integer> predicateCondition) {
+        /*int result = 0;
         for (int value : numbers) {
-            if (condition.test(value))
+            if (predicateCondition.test(value))
                 result += value;
         }
-        return result;
+        return result;*/
+
+        return numbers.stream()
+                .filter(predicateCondition)
+                .reduce(0, Integer::sum);
     }
 
     private static int totalEvenValues(List<Integer> numbers) {
---
+++ DesignPatternsJava8/Util.java
@@ -0,0 +1,14 @@
+package DesignPatternsJava8;
+
+/**
+ * Created by Syril on 05-05-2018.
+ */
+public class Util {
+    static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+}
