3fb5e70cc24ff89382ed634fac951478c2bd759f
Syril <syrilster@gmail.com>
2018-05-06 16:32:43 +0530

    Fluent Interfaces

---
+++ DesignPatternsJava8/CascadeMethodPattern/CascadeMethodPattern.java
@@ -0,0 +1,29 @@
+package DesignPatternsJava8.CascadeMethodPattern;
+
+/**
+ * Created by Syril on 06-05-2018.
+ */
+public class CascadeMethodPattern {
+    public static void main(String[] args) {
+        /*Mailer mailer = new Mailer();
+        mailer.from("java.developer.com");
+        mailer.to("syril.sadasivan@gmail.com");
+        mailer.subject("Your code Rocks !!");
+        mailer.body(".. Here is why ..");
+        mailer.send();*/
+
+        //Cascade Method pattern form aka builder pattern (Noise has been removed)
+        /*new Mailer().from("java.developer.com")
+                .to("syril.sadasivan@gmail.com")
+                .subject("Your code Rocks !!")
+                .body(".. Here is why ..")
+                .send();*/
+
+        //Now that send has been made s static method and send method is accepting a Consumer
+        //Sending a Lambda to send method which then gives back some values which is used by the consumer
+        Mailer.send(mailer -> mailer.from("java.developer.com")
+                .to("syril.sadasivan@gmail.com")
+                .subject("Your code Rocks !!")
+                .body(".. Here is why .."));
+    }
+}
---
+++ DesignPatternsJava8/CascadeMethodPattern/Mailer.java
@@ -0,0 +1,40 @@
+package DesignPatternsJava8.CascadeMethodPattern;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by Syril on 06-05-2018.
+ */
+public class Mailer {
+    public static void print(String message) {
+        System.out.println(message);
+    }
+
+    public Mailer from(String address) {
+        print("from");
+        return this;
+    }
+
+    public Mailer to(String address) {
+        print("to");
+        return this;
+    }
+
+    public Mailer subject(String message) {
+        print("subject");
+        return this;
+    }
+
+    public Mailer body(String message) {
+        print("body");
+        return this;
+    }
+
+    public static void send(Consumer<Mailer> block) {
+        /*Now think of this Mailer as a costly resource and the author has decided not to give a reference back to the caller.
+        instead the object is going to die here and this brings in fluency as there is no burden on the caller. (Separation of concerns as well)*/
+        Mailer mailer = new Mailer();
+        block.accept(mailer);
+        System.out.println("Sending...");
+    }
+}
