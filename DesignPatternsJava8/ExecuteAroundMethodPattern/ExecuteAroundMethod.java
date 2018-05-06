afefe9d79dd2535d284aee239d92a472e0a271ee
Syril <syrilster@gmail.com>
2018-05-06 17:06:05 +0530

    Execute Around method pattern

---
+++ DesignPatternsJava8/ExecuteAroundMethodPattern/ExecuteAroundMethod.java
@@ -0,0 +1,33 @@
+package DesignPatternsJava8.ExecuteAroundMethodPattern;
+
+/**
+ * Created by Syril on 06-05-2018.
+ */
+public class ExecuteAroundMethod {
+    public static void main(String[] args) {
+        //Resource resource = new Resource();
+        /* Problems: Easy to forget close and when there is an exception close wont be reached.
+        resource.op1();
+        resource.op2();
+        resource.close();*/
+
+        //This is more verbose
+        /*try {
+            resource.op1();
+            resource.op2();
+        } finally {
+            resource.close();
+        }*/
+
+        //Resource now implements AutoCloseable marker interface(Automatic Resource Management)
+        /*try (Resource resource = new Resource()) {
+            resource.op1();
+            resource.op2();
+        }*/
+
+        //Caller has lot of pre amd post steps to do for the code he does not handle. Lets use a execute around pattern.
+        //Use when boiler plate code is there in the use and other are called like below
+        Resource.use(resource -> resource.op1()
+                                         .op2());
+    }
+}
---
+++ DesignPatternsJava8/ExecuteAroundMethodPattern/Resource.java
@@ -0,0 +1,33 @@
+package DesignPatternsJava8.ExecuteAroundMethodPattern;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by Syril on 06-05-2018.
+ */
+public class Resource implements AutoCloseable {
+    private Resource() {
+        System.out.println("Created..");
+    }
+
+    public Resource op1() {
+        System.out.println("OP1");
+        return this;
+    }
+
+    public Resource op2() {
+        System.out.println("OP2");
+        return this;
+    }
+
+    public void close() {
+        System.out.println("Cleaning..");
+    }
+
+    public static void use(Consumer<Resource> block) {
+        //This is the right way to handle exceptions and resource closure when exposed as API.
+        try (Resource resource = new Resource()) {
+            block.accept(resource);
+        }
+    }
+}
